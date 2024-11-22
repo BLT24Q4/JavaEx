1271934f4e2106b94f8e57c35dcf381575e35354
BLT24Q4 <blt24q4@gmail.com>
2024-11-22 14:50:41 +0900

    Practice08-5

---
+++ src/com/javaex/practice08/ex05/Div.java
@@ -0,0 +1,17 @@
+package com.javaex.practice08.ex05;
+
+public class Div {
+
+    private int a;
+    private int b;
+    
+    public void setValue(int a, int b) {
+    	this.a = a;
+    	this.b = b;
+    }
+
+    public int calculate() {
+    	return a / b;
+    }
+
+}
